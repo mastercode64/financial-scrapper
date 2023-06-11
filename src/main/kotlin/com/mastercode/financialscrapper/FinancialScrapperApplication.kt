2d180e0c9864150fa965afaae9936504e9fa3307
Marcos Fujihashi <marcosfujihashi@gmail.com>
2023-06-10 23:43:12 -0300

    project setup

---
+++ src/main/kotlin/com/mastercode/financialscrapper/FinancialScrapperApplication.kt
@@ -0,0 +1,11 @@
+package com.mastercode.financialscrapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialScrapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancialScrapperApplication>(*args)
+}
