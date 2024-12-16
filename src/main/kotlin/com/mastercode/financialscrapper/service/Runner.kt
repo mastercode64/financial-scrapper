b6653d3f50c340f8f432e6d0fa8754478dd82284
Marcos Fujihashi <marcosfujihashi@gmail.com>
2024-12-15 21:00:56 -0300

    refactor

--- src/main/kotlin/com/mastercode/financialscrapper/service/Runner.kt
+++ src/main/kotlin/com/mastercode/financialscrapper/service/Runner.kt
@@ -1,19 +1,27 @@
 package com.mastercode.financialscrapper.service
 
+import com.mastercode.financialscrapper.csv.CsvHelper
 import com.mastercode.financialscrapper.scrapper.Scrapper
 import com.mastercode.financialscrapper.utils.Formatter
+import com.opencsv.CSVWriter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.io.StringWriter
+import java.nio.charset.StandardCharsets
 import java.util.logging.Logger
 
 @Component
 class Runner(
     @Value("\${STOCKS:#{null}}")
     private val stockParam: String? = null,
-
+    @Value("\${REQUEST_INTERVAL_MS}")
+    private val requestIntervalMs: Long,
     private val scrapper: Scrapper,
+    private val csvHelper: CsvHelper,
 ) : ApplicationRunner {
     companion object {
         private val log: Logger = Logger.getLogger(this::class.qualifiedName)
@@ -21,16 +29,21 @@ class Runner(
 
     override fun run(args: ApplicationArguments) {
         log.info("Starting scrapper")
+        log.info("Using $requestIntervalMs ms interval between requests")
+        requireNotNull(stockParam) { "STOCK parameter cannot be null" }
 
-        requireNotNull(stockParam) { "stock param cannot be null" }
-
-        val stockList = Formatter.commaToList(stockParam)
+        val stockList = Formatter.commaToList(stockParam).sorted()
         log.info("Detected: ${stockList.size} stock(s). $stockList")
 
-        stockList
-            .map(scrapper::getStockInfo)
-            .map { it.toString() }
-            .forEach(log::info)
-        log.info("Finished processing")
+        val stocks = stockList.map {
+            val info = scrapper.getStockInfo(it)
+            log.info("Waiting..")
+            Thread.sleep(requestIntervalMs)
+            log.info("Finished")
+            info
+        }
+
+        log.info("Creating CSV file..")
+        csvHelper.createFile(stocks)
     }
 }
